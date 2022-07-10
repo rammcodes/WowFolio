3c3610b6550569fcdef309d334658e16cbda8d69
Ram Maheshwari <ram991011@gmail.com>
2022-07-10 11:50:58 +0530

    updated small menu

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 const smMenuBtn = document.querySelector('.main-header__sm-scr-nav-btn')
 const smMenu = document.querySelector('.main-header__sm-menu')
 
+const smMenuLinks = document.querySelectorAll('.main-header__sm-menu-link')
 const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1')
 const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2')
 const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3')
 const smMenuLink4 = document.querySelector('.main-header__sm-menu-link--4')
 
 smMenuBtn.addEventListener('click', () => {
-  if (smMenu.classList.contains('main-header__sm-menu--active')) {
+  smMenu.style.transitionDelay = '0s'
+  smMenu.classList.add('main-header__sm-menu--active')
+
+  smMenuLink1.style.transitionDelay = '.5s'
+  smMenuLink1.style.transform = 'translateY(0)'
+  smMenuLink1.style.opacity = '1'
+
+  smMenuLink2.style.transitionDelay = '.8s'
+  smMenuLink2.style.transform = 'translateY(0)'
+  smMenuLink2.style.opacity = '1'
+
+  smMenuLink3.style.transitionDelay = '1.1s'
+  smMenuLink3.style.transform = 'translateY(0)'
+  smMenuLink3.style.opacity = '1'
+
+  smMenuLink4.style.transitionDelay = '1.4s'
+  smMenuLink4.style.transform = 'translateY(0)'
+  smMenuLink4.style.opacity = '1'
+})
+
+smMenuLinks.forEach((ele) => {
+  ele.addEventListener('click', () => {
+    smMenuLink4.style.transitionDelay = '0s'
+    smMenuLink4.style.transform = 'translateY(50px)'
+    smMenuLink4.style.opacity = '0'
+
+    smMenuLink3.style.transitionDelay = '.3s'
+    smMenuLink3.style.transform = 'translateY(50px)'
+    smMenuLink3.style.opacity = '0'
+
+    smMenuLink2.style.transitionDelay = '.6s'
+    smMenuLink2.style.transform = 'translateY(50px)'
+    smMenuLink2.style.opacity = '0'
+
+    smMenuLink1.style.transitionDelay = '.9s'
+    smMenuLink1.style.transform = 'translateY(50px)'
+    smMenuLink1.style.opacity = '0'
+
+    smMenu.style.transitionDelay = '1.2s'
     smMenu.classList.remove('main-header__sm-menu--active')
-  } else {
-    smMenu.classList.add('main-header__sm-menu--active')
 
-    smMenuLink1.style.animation = 'smMenuLinkStartAnimation 1s .5s'
-    smMenuLink1.style.animationFillMode = 'forwards';
-
-    smMenuLink2.style.animation = 'smMenuLinkStartAnimation 1s .8s'
-    smMenuLink2.style.animationFillMode = 'forwards';
-
-    smMenuLink3.style.animation = 'smMenuLinkStartAnimation 1s 1.1s'
-    smMenuLink3.style.animationFillMode = 'forwards';
-
-    smMenuLink4.style.animation = 'smMenuLinkStartAnimation 1s 1.4s'
-    smMenuLink4.style.animationFillMode = 'forwards';
-  }
+    setTimeout(() => {
+      console.log('eelelle', ele.name)
+      document.getElementById(ele.name).scrollIntoView()
+    }, 1300)
+  })
 })
