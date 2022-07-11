2ba2369a309b61ea89185c310531292b62961846
Ram Maheshwari <ram991011@gmail.com>
2022-07-11 11:37:52 +0530

    added close small menu button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const smMenuBtn = document.querySelector('.main-header__sm-scr-nav-btn')
 const smMenu = document.querySelector('.main-header__sm-menu')
+const smMenuCloseBtn = document.querySelector('.main-header__sm-menu-close')
 
 const smMenuLinks = document.querySelectorAll('.main-header__sm-menu-link')
 const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1')
@@ -54,4 +55,25 @@ smMenuLinks.forEach((ele) => {
       document.getElementById(ele.name).scrollIntoView()
     }, 1300)
   })
+})
+
+smMenuCloseBtn.addEventListener('click', () => {
+  smMenuLink4.style.transitionDelay = '0s'
+  smMenuLink4.style.transform = 'translateY(50px)'
+  smMenuLink4.style.opacity = '0'
+
+  smMenuLink3.style.transitionDelay = '.3s'
+  smMenuLink3.style.transform = 'translateY(50px)'
+  smMenuLink3.style.opacity = '0'
+
+  smMenuLink2.style.transitionDelay = '.6s'
+  smMenuLink2.style.transform = 'translateY(50px)'
+  smMenuLink2.style.opacity = '0'
+
+  smMenuLink1.style.transitionDelay = '.9s'
+  smMenuLink1.style.transform = 'translateY(50px)'
+  smMenuLink1.style.opacity = '0'
+
+  smMenu.style.transitionDelay = '1.2s'
+  smMenu.classList.remove('main-header__sm-menu--active')
 })
