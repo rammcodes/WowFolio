51fddc5d3f8c6d901a804b8280b3207f59606582
Ram <ramdevukd@gmail.com>
2023-01-02 16:41:06 +0530

    added theme color selector and updated heading main text

--- script.js
+++ script.js
@@ -77,3 +77,59 @@ smMenuCloseBtn.addEventListener('click', () => {
   smMenu.style.transitionDelay = '1.2s'
   smMenu.classList.remove('main-header__sm-menu--active')
 })
+
+
+
+
+
+// ---
+const themeColorSelector = document.querySelector('.themeClrSelector')
+const themeColorSelectorInput = document.querySelector(
+  '.themeClrSelector__input'
+)
+const root = document.documentElement;
+
+
+
+const hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null
+}
+
+const eventFire = (el, etype) => {
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype)
+  } else {
+    let evObj = document.createEvent('Events')
+    evObj.initEvent(etype, true, false)
+    el.dispatchEvent(evObj)
+  }
+}
+
+themeColorSelector.addEventListener('click', () => {
+  eventFire(themeColorSelectorInput, 'input')
+})
+
+const setDynamicColor = (color) => {
+
+  const { r, g, b } = hexToRgb(`${color}`)
+  
+  root.style.setProperty('--themeColor', `${r},${g},${b}`);
+  //localStorage.setItem('color', color)
+}
+
+themeColorSelectorInput.addEventListener('input', (e) => {
+  setDynamicColor(e.target.value)
+})
+
+// if (localStorage.getItem('color')) {
+//   let userSelectedColor = localStorage.getItem('color')
+//   themeColorSelectorInput.value = userSelectedColor
+//   setDynamicColor(userSelectedColor)
+// }
