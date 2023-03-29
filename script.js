5d15dc41f6ea7bda06a0016e95da80c97092522c
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2023-03-29 11:49:16 +0430

    improving links, js and some styling

--- script.js
+++ script.js
@@ -3,83 +3,52 @@ const smMenu = document.querySelector('.main-header__sm-menu')
 const smMenuCloseBtn = document.querySelector('.main-header__sm-menu-close')
 
 const smMenuLinks = document.querySelectorAll('.main-header__sm-menu-link')
-const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1')
-const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2')
-const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3')
-const smMenuLink4 = document.querySelector('.main-header__sm-menu-link--4')
+const smMenuLink1 = smMenuLinks[0]
+const smMenuLink2 = smMenuLinks[1]
+const smMenuLink3 = smMenuLinks[2]
+const smMenuLink4 = smMenuLinks[3]
+
+const DELAY = .3; // delay between links animation (.3s)
 
 smMenuBtn.addEventListener('click', () => {
   smMenu.style.transitionDelay = '0s'
   smMenu.classList.add('main-header__sm-menu--active')
 
-  smMenuLink1.style.transitionDelay = '.5s'
-  smMenuLink1.style.transform = 'translateY(0)'
-  smMenuLink1.style.opacity = '1'
-
-  smMenuLink2.style.transitionDelay = '.8s'
-  smMenuLink2.style.transform = 'translateY(0)'
-  smMenuLink2.style.opacity = '1'
-
-  smMenuLink3.style.transitionDelay = '1.1s'
-  smMenuLink3.style.transform = 'translateY(0)'
-  smMenuLink3.style.opacity = '1'
-
-  smMenuLink4.style.transitionDelay = '1.4s'
-  smMenuLink4.style.transform = 'translateY(0)'
-  smMenuLink4.style.opacity = '1'
+  for(let i = 0;i < smMenuLinks.length;i++){
+    let link = smMenuLinks[i]
+    link.style.transitionDelay = `${.5 + (i * DELAY)}s`
+    link.style.transform = 'translateY(0)'
+    link.style.opacity = '1'
+  }
 })
 
-smMenuLinks.forEach((ele) => {
-  ele.addEventListener('click', () => {
-    smMenuLink4.style.transitionDelay = '0s'
-    smMenuLink4.style.transform = 'translateY(50px)'
-    smMenuLink4.style.opacity = '0'
-
-    smMenuLink3.style.transitionDelay = '.3s'
-    smMenuLink3.style.transform = 'translateY(50px)'
-    smMenuLink3.style.opacity = '0'
-
-    smMenuLink2.style.transitionDelay = '.6s'
-    smMenuLink2.style.transform = 'translateY(50px)'
-    smMenuLink2.style.opacity = '0'
-
-    smMenuLink1.style.transitionDelay = '.9s'
-    smMenuLink1.style.transform = 'translateY(50px)'
-    smMenuLink1.style.opacity = '0'
-
+smMenuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    for(let i = 0;i < smMenuLinks.length;i++){
+      let link = smMenuLinks[i]
+      link.style.transitionDelay = `${(smMenuLinks.length -1 -i) * DELAY}s`
+      link.style.transform = 'translateY(50px)'
+      link.style.opacity = '0'
+    }
     smMenu.style.transitionDelay = '1.2s'
     smMenu.classList.remove('main-header__sm-menu--active')
 
     setTimeout(() => {
-      document.getElementById(ele.name).scrollIntoView()
+      document.getElementById(link.name).scrollIntoView()
     }, 1300)
   })
 })
 
 smMenuCloseBtn.addEventListener('click', () => {
-  smMenuLink4.style.transitionDelay = '0s'
-  smMenuLink4.style.transform = 'translateY(50px)'
-  smMenuLink4.style.opacity = '0'
-
-  smMenuLink3.style.transitionDelay = '.3s'
-  smMenuLink3.style.transform = 'translateY(50px)'
-  smMenuLink3.style.opacity = '0'
-
-  smMenuLink2.style.transitionDelay = '.6s'
-  smMenuLink2.style.transform = 'translateY(50px)'
-  smMenuLink2.style.opacity = '0'
-
-  smMenuLink1.style.transitionDelay = '.9s'
-  smMenuLink1.style.transform = 'translateY(50px)'
-  smMenuLink1.style.opacity = '0'
-
+  for(let i = 0;i < smMenuLinks.length;i++){
+    let link = smMenuLinks[i]
+    link.style.transitionDelay = `${(smMenuLinks.length -1 -i) * DELAY}s`
+    link.style.transform = 'translateY(50px)'
+    link.style.opacity = '0'
+  }
   smMenu.style.transitionDelay = '1.2s'
   smMenu.classList.remove('main-header__sm-menu--active')
 })
-
-
-
-
 
 // ---
 const themeColorSelector = document.querySelector('.themeClrSelector')
@@ -87,8 +56,6 @@ const themeColorSelectorInput = document.querySelector(
   '.themeClrSelector__input'
 )
 const root = document.documentElement;
-
-
 
 const hexToRgb = (hex) => {
   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
